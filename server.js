c8c6fbed4ab7c97dd3fd103e76870f6b47b8a320
root <root@ip-172-31-31-207.us-west-2.compute.internal>
2021-02-23 01:05:19 +0000

    small change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello guy's, welcome to this new node-js sample application deployment in aws');
 });
 
 app.listen(PORT, HOST);
