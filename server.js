b8f0df7f9b53619c31967e219f5dd8953d09158d
KRajesh22 <37106586+KRajesh22@users.noreply.github.com>
2021-03-10 03:07:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 7070;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
+// server.js
+const express = require('express')
+const app = express()
 app.get('/', (req, res) => {
-  res.send('Hello guy's, welcome to this new node-js sample application deployment in aws');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+    res.send('Hello Springworks!!')
+})
+app.listen(8080, () => {
+    console.log('Server is up on 8080')
+})
